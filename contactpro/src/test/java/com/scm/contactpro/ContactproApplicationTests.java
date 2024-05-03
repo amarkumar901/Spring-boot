280b098f40c220b5473e5d0689830182164368e7
Amar kumar <amar.kumar@robomq.io>
2024-05-03 14:06:40 +0530

    New project contactpro first commit

---
+++ contactpro/src/test/java/com/scm/contactpro/ContactproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.contactpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
