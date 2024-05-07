671013c477437a8ea4185271b2f9721db970847f
Amar kumar <amar.kumar@robomq.io>
2024-05-07 20:42:15 +0530

    Understanding fragments

---
+++ contactpro/src/main/java/com/scm/ContactproApplication.java
@@ -0,0 +1,13 @@
+package com.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactproApplication.class, args);
+	}
+
+}
--- contactpro/src/main/java/com/scm/controllers/PageController.java
+++ contactpro/src/main/java/com/scm/controllers/PageController.java
@@ -1,14 +1,35 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("Home page handler");
+
+        //Sending data to view
+        model.addAttribute("name", "Substring technologies.");
+        model.addAttribute("youtubeChannel", "Amar kumar");
+        model.addAttribute("github", "technologies.");
         return "home";
+    }
+
+    //About route
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", false);
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    //Services
+    @RequestMapping("/services")
+    public String servicesPage(){
+        System.out.println("Services page loading");
+        return "services";
     }
 }
