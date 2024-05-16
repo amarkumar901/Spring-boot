a8661a80e576b46a979b15884c39158524dba207
Amar kumar <amar.kumar@robomq.io>
2024-05-17 01:21:05 +0530

    Signup backend implementation

--- contactpro/src/main/java/com/scm/controllers/PageController.java
+++ contactpro/src/main/java/com/scm/controllers/PageController.java
@@ -3,7 +3,12 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scm.forms.UserForm;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
@@ -48,9 +53,23 @@ public class PageController {
     }
 
     @GetMapping("register")
-    public String register() {
+    public String register(Model model) {
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
         return "register";
     }
     
+    //processing register
+    @RequestMapping(value="/do-register", method = RequestMethod.POST)
+    public String processRegister(@ModelAttribute UserForm userForm){
+        System.out.println("Processing registration.");
+        // fetch the form data
+        System.out.println(userForm);
+        // validate form data
+        // save to database
+        //message = "Registration successful"
+        // redirect to login page
+        return "redirect:/register";
+    }
     
 }
